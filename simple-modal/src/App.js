c8bf6bcef1e0b534758a6fb1d3d999134636d3a2
dave <lklone2005@gmail.com>
2020-04-07 00:14:24 +0900

    add Clock with 'componentDidMount'

--- simple-modal/src/App.js
+++ simple-modal/src/App.js
@@ -8,8 +8,28 @@ import Clock from './components/Clock'
 // import Todolist from './components/Todolist'
 // import Weather from './components/Weather'
 
+function runClock () {
+  const clock = document.querySelector('#root .App #clock');
+  const clockTitle = clock.querySelector('.container .clock');
+  
+  const date = new Date();
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
+  const seconds = date.getSeconds();
+
+  clockTitle.innerText = `${hours < 10 ? `0${hours}` : hours} : ${minutes < 10 ? `0${minutes}` : minutes} : ${seconds < 10 ? `0${seconds}` : seconds}`;
+}
+
+function init() {
+  runClock();
+  setInterval(runClock, 1000);
+}
 
 class App extends Component {
+  componentDidMount(){
+    init();
+  }
+  
   render() {
     return (
       <div className="App">
--- simple-modal/src/components/Clock.js
+++ simple-modal/src/components/Clock.js
@@ -1,38 +1,22 @@
 import React, { Component } from 'react';
-
-function runClock() {
-    const clock = document.querySelector('#root .App clock #clock');
-    console.log(clock);
-    const clockTitle = clock.querySelector('.container .clock');
-    console.log(clockTitle)
-
-    const date = new Date();
-    const hours = date.getHours();
-    const minutes = date.getMinutes();
-    const seconds = date.getSeconds();
-
-    clockTitle.innerText = `${hours < 10 ? `0${hours}` : hours} : ${minutes < 10 ? `0${minutes}` : minutes} : ${seconds < 10 ? `0${seconds}` : seconds}`;
-}
-
-function init() {
-    runClock();
-    setInterval(runClock, 1000);
-}
-
+console.log('this is Clock component')
 class Clock extends Component {
     render() {
-        console.log('this is Clock.js - Third')
         return(
-            <clock>
-                <div id="clock">
-                    <div class="container">
-                        <div class="clock" onLoad = {() => {
-                            init();
-                        }}>                            
-                        </div>
+            <div id="clock">
+                <div className="container">
+                    <div className="clock" 
+                    // onLoad={function(e) {
+                    //     e.preventDefault();
+                    //     this.props.onChangePage();
+                    //     console.log('This is onLoad props');
+                    // }.bind(this)}
+                    >
+                        <p>Hello React!!!</p>  
+                                             
                     </div>
                 </div>
-            </clock>
+            </div>
         )
     }
 }
