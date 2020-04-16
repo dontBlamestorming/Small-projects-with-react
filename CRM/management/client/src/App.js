bc5b409144464d66502e59a9c01374f26861b481
dave <lklone2005@gmail.com>
2020-04-16 19:58:54 +0900

    add get customers REST API module

--- CRM/management/client/src/App.js
+++ CRM/management/client/src/App.js
@@ -22,34 +22,27 @@ const styles = theme => ({ // css값마저 객체형태로 전달하네
   }
 })
 
- const customers = [
-{
-  'id' : 1,
-  'image' : 'https://placeimg.com/64/64/1',
-  'name' : '박상균',
-  'birthday' : '961222',
-  'gender' : '남자',
-  'job' : '대학생'
-},
-{
-  'id' : 2,
-  'image' : 'https://placeimg.com/64/64/2',
-  'name' : '홍길동',
-  'birthday' : '960124',
-  'gender' : '남자',
-  'job' : '프로그래머'
-},
-{
-  'id' : 3,
-  'image' : 'https://placeimg.com/64/64/3',
-  'name' : '김단아',
-  'birthday' : '910419',
-  'gender' : '남자',
-  'job' : '디자이너'
-},
-]
-
+// component는 라이브러리로서 생명주기가 있다.
 class App extends Component {
+  // 고객정보는 고정된 것이 아니라 사용자의 요청에 따라 바뀌는 값이다. 
+  state = {
+    customers : ""
+  }
+
+  componentDidMount() {
+    this.callApi()
+      .then(res => this.setState({  // returned 'body' in callApi
+        customers : res
+      }))
+      .catch(err => console.log(err));
+  }
+ 
+  callApi = async () => {
+    const response = await fetch('/api/customers');
+    const body = await response.json();
+    return body;
+  }
+
   render() {
     const { classes } = this.props; 
     return (
@@ -65,21 +58,22 @@ class App extends Component {
               <TableCell>직업</TableCell>
             </TableRow>
           </TableHead>
-          <TableBody> { 
-            customers.map(c => {
-              return (
-                <Customer 
-                  key = {c.id}
-                  id = {c.id} 
-                  image = {c.image} 
-                  name = {c.name} 
-                  birthday = {c.birthday} 
-                  gender = {c.gender} 
-                  job = {c.job}>
-                </Customer>
-              );
-            })
-          }
+          <TableBody> 
+            { 
+              this.state.customers ? this.state.customers.map(c => {
+                return (
+                  <Customer 
+                    key = {c.id}
+                    id = {c.id} 
+                    image = {c.image} 
+                    name = {c.name} 
+                    birthday = {c.birthday} 
+                    gender = {c.gender} 
+                    job = {c.job}>
+                  </Customer>
+                );
+              }) : ""
+            }
           </TableBody>
         </Table>
       </Paper>
@@ -91,4 +85,6 @@ export default withStyles(styles)(App);
 
 /*
 각각의 컴포넌트가 포함하고 있는 내용을 분리 - 관리, 생산성
+
+react의 오류중 warning인 경우는 동작에는 크게 문제는 없다는 뜻, 그럼 다른 문제가 없는지를 봐야 하는데 network를 한번 살펴보자. 
 */
