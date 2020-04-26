7a8556f74e587c620938e8d6bdf6af674c274d18
dave <lklone2005@gmail.com>
2020-04-26 16:50:59 +0900

    materual design for customer add/delete

--- CRM/management/client/src/components/CustomerAdd.js
+++ CRM/management/client/src/components/CustomerAdd.js
@@ -1,6 +1,20 @@
 import React, { Component } from 'react';
 import { post } from 'axios';
 
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import DialogContent from '@material-ui/core/DialogContent';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = theme => ({
+    hidden : {
+        display : 'none'
+    }
+});
+
 class CustomerAdd extends Component {
     constructor(props) {
         super(props);
@@ -10,7 +24,8 @@ class CustomerAdd extends Component {
             birthday : '',
             gender : '',
             job : '',
-            fileName : ''
+            fileName : '',
+            open : false    // dialog 창이 열려있는지를 확인
         }
     }
 
@@ -34,6 +49,24 @@ class CustomerAdd extends Component {
         return post(url, formData, config); // axios 
     }
 
+    handleClickOpen = () => {   // 바인딩처리?
+        this.setState({
+            open : true
+        });
+    }
+
+    handleClose = () => {
+        this.setState({
+            file : null,
+            userName : '',
+            birthday : '',
+            gender : '',
+            job : '',
+            fileName : '',
+            open : false 
+        })
+    }
+
     handleFormSubmit = (e) => {
         e.preventDefault();
         this.addCustomer()
@@ -47,7 +80,8 @@ class CustomerAdd extends Component {
             birthday : '',
             gender : '',
             job : '',
-            fileName : ''
+            fileName : '',
+            open : false
         })
     }
 
@@ -65,9 +99,35 @@ class CustomerAdd extends Component {
         this.setState(nextState);
     }
 
-
     render() {
+        const { classes } = this.props;
         return (
+            <div>
+                <Button variant = "contained" color = "primary" onClick = {this.handleClickOpen}>
+                    고객 추가하기
+                </Button>
+                <Dialog open = {this.state.open} onClose = {this.handleClose}>
+                    <DialogTitle>고객 추가</DialogTitle>
+                    <DialogContent>
+                        <input className = {classes.hidden} accept = "image/*" id = "raised-button-file" type = "file" file = {this.state.file} value = {this.state.fileName} onChange = {this.handleFileChange}/><br/>
+                        <label htmlFor = "raised-button-file">
+                            <Button variant = "contained" color = "primary" component = "span" name = "file">
+                                {this.state.fileName === "" ? "프로필 이미지 선택" : this.state.fileName}
+                            </Button>
+                        </label>
+                        <br/>
+                        <TextField label="이름" type = "text" name = "userName" value = {this.state.userName} onChange = {this.handleValueChange}/><br/>
+                        <TextField label="생년월일" type = "text" name = "birthday" value = {this.state.birthday} onChange = {this.handleValueChange}/><br/>
+                        <TextField label="성별" type = "text" name = "gender" value = {this.state.gender} onChange = {this.handleValueChange}/><br/>
+                        <TextField label="직업" type = "text" name = "job" value = {this.state.job} onChange = {this.handleValueChange}/><br/>
+                    </DialogContent>
+                    <DialogActions>
+                        <Button variant = "contained" color = "primary" onClick = {this.handleFormSubmit}>추가</Button>
+                        <Button variant = "outlined" color = "primary" onClick = {this.handleClose}>닫기</Button>
+                    </DialogActions>
+                </Dialog>
+            </div>
+            /*
             <form onSubmit = {this.handleFormSubmit}>
                 <h1>고객 추가</h1>
                 프로필 이미지 : <input type = "file" name = "file" file = {this.state.file} value = {this.state.fileName} onChange = {this.handleFileChange}/><br/>
@@ -77,11 +137,12 @@ class CustomerAdd extends Component {
                 직업 : <input type = "text" name = "job" value = {this.state.job} onChange = {this.handleValueChange}/><br/>
                 <button type = "submit">추가하기</button>
             </form>
+            */
         )
     }
 }
 
-export default CustomerAdd;
+export default withStyles(styles)(CustomerAdd);
 
 
 // 이름, 나이, 성별 등 바뀔 수 있는 정보는 state 변수를 일단 초기화한다.
