4f04016b40cd389a4e7a5b94e04aedecef428340
dave <lklone2005@gmail.com>
2020-07-15 20:52:27 +0900

    connect data from external API

--- news-viewer/src/App.js
+++ news-viewer/src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import NewsList from './components/NewsList';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  return <NewsList />;
+};
+// import axios from 'axios';
+
+// const App = () => {
+//   const [data, setData] = useState(null);
+//   const key = 'f37687b691e74a32a8ddaa8736edf8f2';
+
+//   /*
+//     const onClick = () => {
+//       axios.get('https://jsonplaceholder.typicode.com/todos/1').then(response => {
+//         setData(response);
+//       });
+//     };
+//   */
+//   const onClick = async () => {
+//     try {
+//       const response = await axios.get(
+//         `https://newsapi.org/v2/top-headlines?country=kr&apiKey=${key}`,
+//       );
+//       setData(response.data);
+//     } catch (e) {
+//       console.log(e);
+//     }
+//   };
+//   return (
+//     <div>
+//       <div>
+//         <button onClick={onClick}>불러오기</button>
+//       </div>
+
+//       {data && (
+//         <textarea
+//           rows={7}
+//           value={JSON.stringify(data, null, 2)}
+//           readOnly={true}
+//         />
+//       )}
+//     </div>
+//   );
+// };
 
 export default App;
