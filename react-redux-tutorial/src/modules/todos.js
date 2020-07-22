88e892eaa988fdad3caa1434eea3339dda544ff0
dave <lklone2005@gmail.com>
2020-07-22 17:23:50 +0900

    change reducer function with produce() of immer

--- react-redux-tutorial/src/modules/todos.js
+++ react-redux-tutorial/src/modules/todos.js
@@ -1,4 +1,5 @@
 import { createAction, handleActions } from 'redux-actions';
+import produce from 'immer';
 
 // action type
 const CHANGE_INPUT = 'todos/CHANGE_INPUT'; // input 값 변경
@@ -11,11 +12,9 @@ export const changeInput = createAction(CHANGE_INPUT, input => input);
 
 let id = 3; // insert가 호출될 때마다 1씩 더해진다.
 export const insert = createAction(INSERT, text => ({
-  todo: {
-    id: id++,
-    text,
-    done: false,
-  },
+  id: id++,
+  text,
+  done: false,
 }));
 
 export const toggle = createAction(TOGGLE, id => id);
@@ -43,21 +42,24 @@ const initialState = {
 // 객체 비구조화 문법으로 action 값의 payload 이름을 새로 설정하면 action.payload가 정확히 어떤 값을 의미하는지 알 수 있다.
 const todos = handleActions(
   {
-    [CHANGE_INPUT]: (state, { payload: input }) => ({ ...state, input }),
-    [INSERT]: (state, { payload: todo }) => ({
-      ...state,
-      todo: state.todos.concat(todo),
-    }),
-    [TOGGLE]: (state, { payload: id }) => ({
-      ...state,
-      todos: state.todos.map(todo =>
-        todo.id === id ? { ...todo, done: !todo.done } : todo,
-      ),
-    }),
-    [REMOVE]: (state, { payload: id }) => ({
-      ...state,
-      todos: state.todos.filter(todo => todo.id !== id),
-    }),
+    [CHANGE_INPUT]: (state, { payload: input }) =>
+      produce(state, draft => {
+        draft.input = input;
+      }),
+    [INSERT]: (state, { payload: todo }) =>
+      produce(state, draft => {
+        draft.todos.push(todo);
+      }),
+    [TOGGLE]: (state, { payload: id }) =>
+      produce(state, draft => {
+        const todo = draft.todos.find(todo => todo.id === id);
+        todo.done = !todo.done;
+      }),
+    [REMOVE]: (state, { payload: id }) =>
+      produce(state, draft => {
+        const index = draft.todos.findIndex(todo => todo.id === id);
+        draft.todos.splice(index, 1);
+      }),
   },
   initialState,
 );
