c7696d61f762d14c6259156f57c58a560b927c9a
dave <lklone2005@gmail.com>
2020-07-27 00:30:53 +0900

    basic of redux middleware

--- redux-middleware/src/index.js
+++ redux-middleware/src/index.js
@@ -1,13 +1,15 @@
 import React from "react";
 import ReactDom from "react-dom";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import rootReducer from "./modules";
+// import loggerMiddleware from "./lib/loggerMiddleware";
+import logger from "redux-logger";
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(logger));
 
 ReactDom.render(
   <Provider store={store}>
